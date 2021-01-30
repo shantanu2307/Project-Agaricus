6491ca122efcf9c02cb320cef947a242bae28d25
shantanu2307 <59186786+shantanu2307@users.noreply.github.com>
2021-01-30 23:21:44 +0530

    update

--- src/components/SellerView.js
+++ src/components/SellerView.js
@@ -10,15 +10,37 @@ export default function SellerView() {
       uid:uid
     });
     console.log(response);
+    
+  }
+  async function handleSubmit2() {
+    const uid = currentUser.uid;
+    const response = await axios.post("/delListing", {
+      uid: uid,
+    console.log(response);
   }
   const {currentUser}=useAuth();
   const [resp,setResp]=useState();
   return (
     <div>
       <Navbar />
-      
-      <Button className="w-25" type="submit" variant="success" onClick={handleSubmit}>
+      <Card>
+        <Card.Header className=" h1 text-center text-uppercase font-weight-bold">My Listing</Card.Header>
+      </Card>
+      <Button
+        className="w-50"
+        type="submit"
+        variant="success"
+        onClick={handleSubmit}
+      >
         Get Listings
+      </Button>
+      <Button
+        className="w-50"
+        type="submit"
+        variant="danger"
+        onClick={handleSubmit2}
+      >
+        Delete Listings
       </Button>
     </div>
   );
