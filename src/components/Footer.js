ef0e5505f8036e2f5993f131511ad273a996df16
Anureet Kaur <anureetkaur204@gmail.com>
2021-01-31 14:36:40 +0530

    footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,25 +1,26 @@
 import React from "react";
 import logo from "../assets/3.png"
+import './login.css'
 export default function Footer() {
 
   return (
-    <div class="footer">
-            <div class="footer__logo-box">
-                <img src={logo} alt="Full logo" class="footer__logo" />
+    <div className="footer">
+            <div className="footer__logo-box">
+                <img src={logo} alt="Full logo" className="footer__logo logo" />
             </div>
-            <div class="row">
-                <div class="col-1-of-2">
-                    <div class="footer__navigation">
-                        <ul class="footer__list">
-                            <li class="footer_item">Company</li>
-                            <li class="footer_item">Contact us</li>
-                            <li class="footer_item">Privacy policy</li>
-                            <li class="footer_item">Terms</li>
+            <div className="row footer-bottom">
+                <div className="col-1-of-2">
+                    <div className="footer__navigation">
+                        <ul className="footer__list">
+                            <div><li className="footer_item">Company</li></div>
+                            <div><li className="footer_item">Contact us</li></div>
+                            <div><li className="footer_item">Privacy policy</li></div>
+                            <div><li className="footer_item">Terms</li></div>
                         </ul>
                     </div>
                 </div>
-                <div class="col-1-of-2">
-                    <p class="footer__copyright">
+                <div className="col-1-of-2">
+                    <p className="footer__copyright">
                         Built by Agaricus Systems Pvt. Ltd
                         Copyright &copy; by AGARICUS
                     </p>
