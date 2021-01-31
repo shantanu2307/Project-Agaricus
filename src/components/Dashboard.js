60834fd2076aedce07429af234fbe2c21ec23407
shantanu2307 <59186786+shantanu2307@users.noreply.github.com>
2021-01-31 11:12:43 +0530

    Update Dashboard.js

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -5,19 +5,8 @@ import { Link, useHistory } from "react-router-dom";
 import axios from "axios";
 import Navbar from './FarmerNavBar'
 export default function Dashboard() {
-  const [error, setError] = useState("");
-  const history = useHistory();
-  const { currentUser, logout } = useAuth();
+  const { currentUser } = useAuth();
   const [listingArray,setListingArray]=useState("");
-  async function handleLogOut() {
-    setError("");
-    try {
-      await logout();
-      history.push("/");
-    } catch (e) {
-      setError("Failed to logout!");
-    }
-  }
   async function handleSubmit(){
     const response =await axios.post('/allListing',{
       state:stateRef.current.value,
@@ -31,36 +20,52 @@ export default function Dashboard() {
   const pincodeRef=useRef();
   return (
     <>
-    <Navbar></Navbar>
+      <Navbar></Navbar>
       <Card>
         <Card.Header className="h1 font-weight-bold text-center text-uppercase bg-white">
           Welcome {currentUser.email}
         </Card.Header>
       </Card>
-      <div className=" mt-3 h2 text-center font-weight-bold">
+      <div className=" mt-3 h2 text-center text-capitalize font-weight-bold">
         Get listings for spores and stubble
       </div>
       {listingArray}
       <Form className="ml-2" onSubmit={handleSubmit}>
-        <Form.Group id="state">
-          <Form.Label className="font-weight-bold">State:</Form.Label>
-          <Form.Control type="email" ref={stateRef} required></Form.Control>
-        </Form.Group>
-        <Form.Group id="district">
-          <Form.Label className="font-weight-bold">District:</Form.Label>
-          <Form.Control type="email" ref={districtRef} required></Form.Control>
-        </Form.Group>
-        <Form.Group id="pincode">
-          <Form.Label className="font-weight-bold">Pincode:</Form.Label>
-          <Form.Control type="email" ref={pincodeRef} required></Form.Control>
-        </Form.Group>
-        <Button type="submit">Submit</Button>
+        <div className="d-flex justify-content-center" style={{ gap: "20%" }}>
+          <Form.Group id="state">
+            <Form.Label className="font-weight-bold">State:</Form.Label>
+            <Form.Control
+              type="email"
+              ref={stateRef}
+              className="w-100"
+              required
+            ></Form.Control>
+          </Form.Group>
+          <Form.Group id="district">
+            <Form.Label className="font-weight-bold">District:</Form.Label>
+            <Form.Control
+              type="email"
+              ref={districtRef}
+              className="w-100"
+              required
+            ></Form.Control>
+          </Form.Group>
+          <Form.Group id="pincode">
+            <Form.Label className="font-weight-bold">Pincode:</Form.Label>
+            <Form.Control
+              type="email"
+              ref={pincodeRef}
+              className="w-100"
+              required
+            ></Form.Control>
+          </Form.Group>
+        </div>
+        <div className="d-flex justify-content-center">
+          <Button type="submit" className="w-25">
+            Submit
+          </Button>
+        </div>
       </Form>
-      <div className="w-100 text-center mt-2">
-        <Button variant="link" onClick={handleLogOut}>
-          Log Out
-        </Button>
-      </div>
     </>
   );
 }
