ba4f6383c980b8861810c28cecc9a0016a043896
shantanu2307 <59186786+shantanu2307@users.noreply.github.com>
2021-01-31 11:36:20 +0530

    Update Dashboard.js

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,23 +1,26 @@
 import React, { useState ,useRef} from "react";
-import { Card, Button, Alert,Form } from "react-bootstrap";
+import { Card, Button,Form } from "react-bootstrap";
 import { useAuth } from "../contexts/AuthContext";
-import { Link, useHistory } from "react-router-dom";
 import axios from "axios";
 import Navbar from './FarmerNavBar'
 export default function Dashboard() {
   const { currentUser } = useAuth();
   const [listingArray,setListingArray]=useState("");
-  async function handleSubmit(){
+  async function handleSubmit(e){
+    e.preventDefault();
+    const details = await axios.get("https://api.postalpincode.in/pincode/"+pincodeRef.current.value);
+    const state=details.data[0].PostOffice[0].State;
+    const district=details.data[0].PostOffice[0].District;
+    console.log(details);
     const response =await axios.post('/allListing',{
-      state:stateRef.current.value,
-      district:districtRef.current.value,
-      pincode:pincodeRef.currenrt.value
+      pincode:pincodeRef.current.value,
+      state:state,
+      district:district
     });
     console.log(response);
   }
-  const stateRef=useRef();
-  const districtRef=useRef();
   const pincodeRef=useRef();
+  const typeRef=useRef();
   return (
     <>
       <Navbar></Navbar>
@@ -32,32 +35,21 @@ export default function Dashboard() {
       {listingArray}
       <Form className="ml-2" onSubmit={handleSubmit}>
         <div className="d-flex justify-content-center" style={{ gap: "20%" }}>
-          <Form.Group id="state">
-            <Form.Label className="font-weight-bold">State:</Form.Label>
-            <Form.Control
-              type="email"
-              ref={stateRef}
-              className="w-100"
-              required
-            ></Form.Control>
-          </Form.Group>
-          <Form.Group id="district">
-            <Form.Label className="font-weight-bold">District:</Form.Label>
-            <Form.Control
-              type="email"
-              ref={districtRef}
-              className="w-100"
-              required
-            ></Form.Control>
-          </Form.Group>
           <Form.Group id="pincode">
             <Form.Label className="font-weight-bold">Pincode:</Form.Label>
             <Form.Control
-              type="email"
+              type="text"
               ref={pincodeRef}
               className="w-100"
               required
             ></Form.Control>
+          </Form.Group>
+          <Form.Group id="type">
+            <Form.Label className="font-weight-bold">Type</Form.Label>
+            <Form.Control ref={typeRef} as="select" defaultValue="Hoticulture">
+              <option>Horticulture</option>
+              <option>Stubble</option>
+            </Form.Control>
           </Form.Group>
         </div>
         <div className="d-flex justify-content-center">
