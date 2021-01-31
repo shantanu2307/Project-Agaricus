f0368ed009455461278d8cce4c297f7bef57e804
shantanu2307 <59186786+shantanu2307@users.noreply.github.com>
2021-01-31 18:39:17 +0530

    Update Dashboard.js

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,5 +1,5 @@
 import React, { useState ,useRef} from "react";
-import { Card, Button,Form } from "react-bootstrap";
+import { Card, Button,Form ,Alert} from "react-bootstrap";
 import { useAuth } from "../contexts/AuthContext";
 import axios from "axios";
 import Navbar from './FarmerNavBar'
@@ -9,38 +9,48 @@ export default function Dashboard() {
   async function handleSubmit(e){
     e.preventDefault();
     const details = await axios.get("https://api.postalpincode.in/pincode/"+pincodeRef.current.value);
-    const state=details.data[0].PostOffice[0].State;
-    const district=details.data[0].PostOffice[0].District;
-    console.log(state,district);
     console.log(details);
-    const response =await axios.post('/allListings',{
-      pincode:pincodeRef.current.value,
-      state:state,
-      district:district,
-      type:typeRef.current.value
-    });
-    console.log(response);
-    const x=response.data.map((listing,index)=>{
-      return(
-        <Card>
-          <Card.Header className="text-center">Listing {index}</Card.Header>
-          <Card.Body>
-            <div >
-              <img src={listing.pics} alt="pic"></img>
-            </div>
-            <div>Name: {listing.sellerName}</div>
-            <div>Type: {listing.type}</div>
-            <div>Details: {listing.details}</div>
-            <div>State: {listing.state}</div>
-            <div>District: {listing.district}</div>
-            <div>Pincode:{listing.pincode}</div>
-          </Card.Body>
-        </Card>
-      );
-    })
-    setEntries(x);
+    if (
+      details.data[0].Message==="No records found"
+    ) {
+      setError((<span>Invalid Pincode</span>))
+      setLoading(true);
+    } else {
+      const state = details.data[0].PostOffice[0].State;
+      const district = details.data[0].PostOffice[0].District;
+      console.log(state, district);
+      console.log(details);
+      const response = await axios.post("/allListings", {
+        pincode: pincodeRef.current.value,
+        state: state,
+        district: district,
+        type: typeRef.current.value,
+      });
+      console.log(response);
+      const x = response.data.map((listing, index) => {
+        return (
+          <Card>
+            <Card.Header className="text-center">Listing {index}</Card.Header>
+            <Card.Body>
+              <div>
+                <img src={listing.pics} alt="pic"></img>
+              </div>
+              <div>Name: {listing.sellerName}</div>
+              <div>Type: {listing.type}</div>
+              <div>Details: {listing.details}</div>
+              <div>State: {listing.state}</div>
+              <div>District: {listing.district}</div>
+              <div>Pincode:{listing.pincode}</div>
+            </Card.Body>
+          </Card>
+        );
+      });
+      setEntries(x);
+    }
   }
   const [entries,setEntries]=useState("");
+  const [error,setError]=useState("");
+  const [loading,setLoading]=useState(false);
   const pincodeRef=useRef();
   const typeRef=useRef();
   return (
@@ -54,7 +64,7 @@ export default function Dashboard() {
       <div className=" h2 text-center text-capitalize font-weight-bold">
         Get listings for spores and stubble
       </div>
-      {listingArray}
+      {loading &&(<Alert variant="danger" className="font-weight-bolder h5 mt-2">{error}</Alert>)}
       <Form className="ml-2" onSubmit={handleSubmit}>
         <div className="d-flex justify-content-center" style={{ gap: "20%" }}>
           <Form.Group id="pincode">
