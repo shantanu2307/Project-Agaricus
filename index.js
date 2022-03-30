1babd82c4fd6cdd023b1ebc9c1f858aac94356ad
shantanu2307 <59186786+shantanu2307@users.noreply.github.com>
2022-03-30 19:32:57 +0530

    Major

---
+++ index.js
@@ -0,0 +1,37 @@
+const express = require("express");
+const morgan = require("morgan");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const cors = require('cors');
+const farmerRoute = require("./server/routes/farmer");
+const sellerRoute = require("./server/routes/seller");
+const listingRoute = require("./server/routes/listing");
+const marketRoute = require("./server/routes/market");
+require("dotenv").config();
+const app = express();
+const uri = process.env.MONGO_URI;
+const port = process.env.PORT || 8080;
+const path = require("path");
+
+
+app.use(morgan("tiny"));
+app.use(bodyParser.json({ limit: '100mb' }));
+app.use(bodyParser.urlencoded({ limit: '100mb', extended: true }));
+app.use(farmerRoute);
+app.use(sellerRoute);
+app.use(listingRoute);
+app.use(marketRoute)
+app.use(cors());
+app.use(express.static(path.join(__dirname, "..", "client", "build")));
+
+mongoose.connect(uri, {
+  useNewUrlParser: "true",
+});
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "build", "index.html"))
+});
+
+app.listen(port, () => {
+  console.log(`Running at Port ${port}`);
+});
--- server/routes/listing.js
+++ server/routes/listing.js
@@ -1,91 +1,74 @@
 const Listing = require("../Models/listings.model");
 const Seller = require("../Models/sellers.model");
 const router = require("express").Router();
-const redis =require('redis');
-const client = redis.createClient({
-  host: "redis-13137.c51.ap-southeast-2-1.ec2.cloud.redislabs.com",
-  port: 13137,
-  password: "QUCxSKxSUmgUX79FW3D2izFqMeFz4LtP",
-});
 router.post("/listing", async (req, res) => {
-    const id=req.body.uid;
-    const seller = await Seller.findOne({ uid: id }).cache({ expire: 10,key:id });
-    var type= "Stubble"
-    if(seller.type==="Horticulture")
+  const id = req.body.uid;
+  const seller = await Seller.findOne({ uid: id });
+  var type = "Stubble"
+  if (seller.type === "Horticulture")
     type = "Horticulture"
-    const obj = {
-      uid: seller._id,
-      district: req.body.district,
-      state: req.body.state,
-      details: req.body.details,
-      pincode: req.body.pincode,
-      sellerName: seller.name,
-      certificate: req.body.certificate,
-      pics: req.body.pics,
-      type: type
-    };
-    let listing = await new Listing(obj);
-    await listing.save();   
-    res.send(listing);
+  const obj = {
+    uid: seller._id,
+    district: req.body.district,
+    state: req.body.state,
+    details: req.body.details,
+    pincode: req.body.pincode,
+    sellerName: seller.name,
+    certificate: req.body.certificate,
+    pics: req.body.pics,
+    type: type
+  };
+  let listing = await new Listing(obj);
+  await listing.save();
+  res.send(listing);
 });
 
 router.post("/listings", async (req, res) => {
-  const id=req.body.uid;
-  const seller = await Seller.findOne({ uid: req.body.uid }).cache({ expire: 10,key:id });
-  console.log(seller);
+  const seller = await Seller.findOne({ uid: req.body.uid });
   if (seller) {
-    const key =String(id)+String('Listing');
-    const listing = await Listing.findOne({ uid: seller._id }).cache({expire:10,key:key});
+    const listing = await Listing.findOne({ uid: seller._id });
     res.send(listing);
-  } 
+  }
   else {
-      res.send({ sellerName: "false" });
+    res.send({ sellerName: "false" });
   }
 });
 
 router.post("/delListing", async (req, res) => {
-    const id=req.body.uid;
-    const seller = await Seller.findOne({ uid: id }).cache({expire:10,key:id});
-    const key = String(id) + String("Listing");
-    const listing = await Listing.findOneAndDelete({ uid: seller._id});
-    if (listing!=null) {
-      await client.del(key, function (err, reply) {
-        console.log(reply); // 1
-      });
-      console.log(listing);
-      res.send({ flag: true });
-    }
-    else{
-      res.send({flag:false});
-    }
-  });
+  const id = req.body.uid;
+  const seller = await Seller.findOne({ uid: id });
+  const listing = await Listing.findOneAndDelete({ uid: seller._id });
+  if (listing != null) {
+    res.send({ flag: true });
+  }
+  else {
+    res.send({ flag: false });
+  }
+});
 
-router.post('/allListings', async(req,res)=>{
-  try{
-   var listingA = await Listing.find({pincode: req.body.pincode, type: req.body.type});
-   console.log(listingA);
-   if(listingA.length>10)
-   {
+router.post('/allListings', async (req, res) => {
+  try {
+    var listingA = await Listing.find({ pincode: req.body.pincode, type: req.body.type });
+    console.log(listingA);
+    if (listingA.length > 10) {
       await res.send(listingA);
-   }
-   var requiredListings = 10 - listingA.length;
-   const listingB =await  Listing.find({district:req.body.district, type: req.body.type,pincode:{$ne: req.body.pincode}}).limit(requiredListings);
+    }
+    var requiredListings = 10 - listingA.length;
+    const listingB = await Listing.find({ district: req.body.district, type: req.body.type, pincode: { $ne: req.body.pincode } }).limit(requiredListings);
 
-   if(listingA.length+listingB.length>10)
-   {
+    if (listingA.length + listingB.length > 10) {
       const combined = listingA.concat(listingB);
       await res.send(combined);
-   }
+    }
     requiredListings = 10 - listingA.length - listingB.length;
-    const listingC = await Listing.find({state:req.body.state, type: req.body.type,pincode : { $ne: req.body.pincode},district:{$ne: req.body.district} }).limit(requiredListings);
+    const listingC = await Listing.find({ state: req.body.state, type: req.body.type, pincode: { $ne: req.body.pincode }, district: { $ne: req.body.district } }).limit(requiredListings);
     const combined = listingA.concat(listingB).concat(listingC)
     console.log(listingC)
     await res.send(combined)
   }
-  catch(e)
-  {
-  console.log(e)
-  await res.send(e)
+  catch (e) {
+    console.log(e)
+    await res.send(e)
   }
 })
 module.exports = router;
